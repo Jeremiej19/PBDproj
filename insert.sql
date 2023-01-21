d2c15a464bb93197b05f86eefb7ae05cfa22e9e7
jciezkowski <jciezkowski@student.agh.edu.pl>
2023-01-21 18:49:48 +0100

    change

--- insert.sql
+++ insert.sql
@@ -8,9 +8,9 @@ INSERT INTO Category VALUES ('Seafood')
 -- Product
 INSERT INTO Product VALUES (1,'Tea',5,1);
 
-INSERT INTO Product VALUES (2,'Water',3,1);
+INSERT INTO Product VALUES (1,'Water',3,1);
 
-INSERT INTO Product VALUES (3,'Lobster',40,3);
+INSERT INTO Product VALUES (3,'Lobster',40,1);
 
 INSERT INTO Menu VALUES ('2023-01-01','2023-01-14',1)
 
